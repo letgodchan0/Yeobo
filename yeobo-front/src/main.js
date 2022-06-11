6f347f0639c79b77f8ec948134d97b0f46f93eb8
herrinekim <herrine.kim@gmail.com>
2022-06-11 21:36:21 +0900

    Add Google Map API

--- yeobo-front/src/main.js
+++ yeobo-front/src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as VueGoogleMaps from "vue2-google-maps" // Import package
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyACs2jbBXhOIlRnugdswVY--7aiBx_D7Ss",
+    libraries: "places",
+    region: "KR"
+  }
+});
 
 new Vue({
   router,
--- yeobo-front/src/views/HomeView.vue
+++ yeobo-front/src/views/HomeView.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="home">
-
+  <div>
+    <gmap-map
+        :zoom="14"    
+        :center="center"
+        style="width:100%;  height: 600px;"
+      >
+    </gmap-map>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
-  name: 'HomeView',
-}
+  name: "HomeView",
+  data() {
+    return {
+      center: { lat: 37.5642135, lng: 127.0016985 },
+      locationMarkers: [],
+      locPlaces: [],
+      existingPlace: null
+    };
+  }, 
+};
 </script>
